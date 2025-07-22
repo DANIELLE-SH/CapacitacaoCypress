b4ffdc47c11536539eb85481c7cf80597ba17654
Soares Haddad <dsoaresh@minsait.com>
2025-07-22 17:53:59 -0300

    Primeiro commit

---
+++ cypress/e2e/Teste.cy.js
@@ -0,0 +1,9 @@
+describe('Teste - Formulário', () => {
+  it('Preencher campos com sucesso', () => { 
+    cy.visit('https://testautomationpractice.blogspot.com/')
+    cy.get('#name').type('Teste')
+    cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com')
+    cy.get('#phone').type('35992560123')
+  })
+
+})
