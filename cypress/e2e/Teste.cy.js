629ae89037c74fb7c6587b33ead055b48fb6fae2
Soares Haddad <dsoaresh@minsait.com>
2025-07-23 17:51:29 -0300

    adicionando mais comandos e iniciando as validacoes

--- cypress/e2e/Teste.cy.js
+++ cypress/e2e/Teste.cy.js
@@ -1,9 +1,24 @@
+/// <reference types="cypress" />
 describe('Teste - Formulário', () => {
-  it('Preencher campos com sucesso', () => { 
-    cy.visit('https://testautomationpractice.blogspot.com/')
-    cy.get('#name').type('Teste')
-    cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com')
-    cy.get('#phone').type('35992560123')
+  xit('Preencher campos com sucesso', () => { 
+      cy.visit('https://testautomationpractice.blogspot.com/')
+      cy.get('#name').type('Teste')
+      cy.get('input[placeholder="Enter EMail"]').type('teste@gmail.com')
+      cy.get('#phone').type('35992560123')
+      cy.get('input[value="female"]').click()
+      cy.get('input[type="checkbox"]').check('sunday')
+      cy.get('#country').select('Canada')
+      cy.get('#colors').select('Red')
+      cy.get('#animals').select('Cat')
+      cy.get('#datepicker').click()
+      cy.get('a[data-date="24"').click()
+      cy.get('#datepicker').should('have.value','07/24/2025')
+      cy.get('#singleFileInput').selectFile('cypress/fixtures/imgTest.png')
+      cy.get('#singleFileInput').should('have.value', 'C:\\fakepath\\imgTest.png')
   })
-
+  it('Validar botao Enter', () => {
+      cy.visit('https://testautomationpractice.blogspot.com/')
+      cy.get('button[name=start]').click()
+      cy.get('button[name="stop"]').should('be.visible')
+  })
 })
